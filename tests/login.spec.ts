a2b7c6d8349212bc5b4bd10fda322a62ceb184f6
Ng Puo Lim <puolim208ng@gmail.com>
2026-07-03 18:49:38 +0800

    test: implement data-driven login tests

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,28 +1,37 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
+import { loginData } from '../data/loginData';
 
-test('Valid Login', async ({ page }) => {
-    const loginPage = new LoginPage(page);
+for(const data of loginData){
+    test(data.title, async ({ page }) => {
+        const loginPage = new LoginPage(page);
+    
+        await loginPage.goto();
+    
+        await loginPage.login(
+            data.username,
+            data.password
+        );
+    
+        if(data.success){
+            await expect(page).toHaveURL(/inventory/);
+    
+            await expect(
+                page.locator('.app_logo')
+            ).toContainText('Swag Labs');
+        
+            await expect(
+                page.locator('.shopping_cart_link')
+            ).toBeVisible();
+        }
+        else{
+            await expect(
+                page.locator('[data-test="error"]')
+            ).toBeVisible();
 
-    await loginPage.goto();
-
-    await loginPage.login(
-        'standard_user',
-        'secret_sauce'
-    );
-
-    await expect(page).toHaveURL(/inventory/);
-
-    await expect(
-        page.locator('.app_logo')
-    ).toContainText('Swag Labs');
-
-    await expect(
-        page.locator('.shopping_cart_link')
-    ).toBeVisible();
-    // await page.goto('https://www.saucedemo.com/');
-    // await page.locator('#user-name').fill('standard_user');
-    // await page.locator('#password').fill('secret_sauce');
-    // await page.locator('#login-button').click();
-    // await expect(page).toHaveURL(/inventory/);
-});
+            await expect(
+                page.locator('[data-test="error"]')
+            ).toContainText('Epic sadface');
+        }
+    });
+}
