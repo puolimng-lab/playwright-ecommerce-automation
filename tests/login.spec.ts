6bf5cf967b5958eaf85c9261405d60350d3e7963
Ng Puo Lim <puolim208ng@gmail.com>
2026-07-04 11:02:20 +0800

    refactor: use beforeEach hook for login tests

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,13 +3,17 @@ import { LoginPage } from '../pages/LoginPage';
 import { InventoryPage } from '../pages/InventoryPage';
 import { loginData } from '../data/loginData';
 
+let loginPage: LoginPage;
+let inventoryPage: InventoryPage;
+
+test.beforeEach(async ({page}) => {
+    loginPage = new LoginPage(page);   
+    inventoryPage = new InventoryPage(page); 
+    await loginPage.goto();   
+});
+
 for(const data of loginData){
-    test(data.title, async ({ page }) => {
-        const loginPage = new LoginPage(page);
-        const inventoryPage = new InventoryPage(page);
-    
-        await loginPage.goto();
-    
+    test(data.title, async () => {
         await loginPage.login(
             data.username,
             data.password
